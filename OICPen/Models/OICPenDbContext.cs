aa0ad0c7d29e8b2dc19d996a4555be498c5c58db
B7202 <B7202@5A12>
2018-10-12 14:07:32 +0900

    Entityframeworkの初期設定完了

---
+++ OICPen/Models/OICPenDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace OICPen.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    class OICPenDbContext : DbContext
+    {
+        public OICPenDbContext()
+            : base("name = OICDbContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<OICPenDbContext, Configuration>());
+        }
+    }
+}
