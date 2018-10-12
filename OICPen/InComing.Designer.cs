8852e9629b6594692ec36056201e147456a73041
b7134 <b7134@5A17>
2018-10-12 16:28:58 +0900

    入庫管理デザイン完成

--- OICPen/InComing.Designer.cs
+++ OICPen/InComing.Designer.cs
@@ -29,118 +29,118 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
-            this.date = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.incomingtitleLbl = new System.Windows.Forms.Label();
+            this.dateLbl = new System.Windows.Forms.Label();
+            this.incomingDtp = new System.Windows.Forms.DateTimePicker();
+            this.incomingDgv = new System.Windows.Forms.DataGridView();
+            this.ordercheckBtn = new System.Windows.Forms.Button();
+            this.itemsLbl = new System.Windows.Forms.Label();
+            this.incomingTbox = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.fixBtn = new System.Windows.Forms.Button();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.incomingDgv)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // incomingtitleLbl
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("UD デジタル 教科書体 N-B", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(1373, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(212, 48);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "入庫管理";
+            this.incomingtitleLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.incomingtitleLbl.AutoSize = true;
+            this.incomingtitleLbl.Font = new System.Drawing.Font("UD デジタル 教科書体 N-B", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.incomingtitleLbl.ForeColor = System.Drawing.Color.Black;
+            this.incomingtitleLbl.Location = new System.Drawing.Point(1373, 19);
+            this.incomingtitleLbl.Name = "incomingtitleLbl";
+            this.incomingtitleLbl.Size = new System.Drawing.Size(212, 48);
+            this.incomingtitleLbl.TabIndex = 0;
+            this.incomingtitleLbl.Text = "入庫管理";
             // 
-            // date
+            // dateLbl
             // 
-            this.date.AutoSize = true;
-            this.date.Font = new System.Drawing.Font("UD デジタル 教科書体 N-B", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.date.Location = new System.Drawing.Point(902, 324);
-            this.date.Name = "date";
-            this.date.Size = new System.Drawing.Size(116, 29);
-            this.date.TabIndex = 1;
-            this.date.Text = "日付  :";
+            this.dateLbl.AutoSize = true;
+            this.dateLbl.Font = new System.Drawing.Font("UD デジタル 教科書体 N-B", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dateLbl.Location = new System.Drawing.Point(902, 324);
+            this.dateLbl.Name = "dateLbl";
+            this.dateLbl.Size = new System.Drawing.Size(116, 29);
+            this.dateLbl.TabIndex = 1;
+            this.dateLbl.Text = "日付  :";
             // 
-            // dateTimePicker1
+            // incomingDtp
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(1113, 324);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(230, 36);
-            this.dateTimePicker1.TabIndex = 2;
+            this.incomingDtp.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.incomingDtp.Location = new System.Drawing.Point(1113, 324);
+            this.incomingDtp.Name = "incomingDtp";
+            this.incomingDtp.Size = new System.Drawing.Size(230, 36);
+            this.incomingDtp.TabIndex = 2;
             // 
-            // dataGridView1
+            // incomingDgv
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(44, 55);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(800, 852);
-            this.dataGridView1.TabIndex = 3;
+            this.incomingDgv.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.incomingDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.incomingDgv.Location = new System.Drawing.Point(44, 55);
+            this.incomingDgv.Name = "incomingDgv";
+            this.incomingDgv.RowTemplate.Height = 21;
+            this.incomingDgv.Size = new System.Drawing.Size(800, 852);
+            this.incomingDgv.TabIndex = 3;
             // 
-            // button1
+            // ordercheckBtn
             // 
-            this.button1.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(907, 209);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(265, 47);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "発注状況確認";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ordercheckBtn.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ordercheckBtn.Location = new System.Drawing.Point(907, 209);
+            this.ordercheckBtn.Name = "ordercheckBtn";
+            this.ordercheckBtn.Size = new System.Drawing.Size(265, 47);
+            this.ordercheckBtn.TabIndex = 4;
+            this.ordercheckBtn.Text = "発注状況確認";
+            this.ordercheckBtn.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // itemsLbl
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("UD デジタル 教科書体 N-B", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(902, 401);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(216, 29);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "商品コード　：";
+            this.itemsLbl.AutoSize = true;
+            this.itemsLbl.Font = new System.Drawing.Font("UD デジタル 教科書体 N-B", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.itemsLbl.Location = new System.Drawing.Point(902, 401);
+            this.itemsLbl.Name = "itemsLbl";
+            this.itemsLbl.Size = new System.Drawing.Size(216, 29);
+            this.itemsLbl.TabIndex = 1;
+            this.itemsLbl.Text = "商品コード　：";
             // 
-            // textBox1
+            // incomingTbox
             // 
-            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.textBox1.Location = new System.Drawing.Point(1113, 401);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(230, 36);
-            this.textBox1.TabIndex = 5;
+            this.incomingTbox.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.incomingTbox.Location = new System.Drawing.Point(1113, 401);
+            this.incomingTbox.Name = "incomingTbox";
+            this.incomingTbox.Size = new System.Drawing.Size(230, 36);
+            this.incomingTbox.TabIndex = 5;
             // 
-            // button2
+            // searchBtn
             // 
-            this.button2.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button2.Location = new System.Drawing.Point(1363, 401);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(85, 42);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "検索";
-            this.button2.UseVisualStyleBackColor = true;
+            this.searchBtn.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.searchBtn.Location = new System.Drawing.Point(1363, 401);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(85, 42);
+            this.searchBtn.TabIndex = 6;
+            this.searchBtn.Text = "検索";
+            this.searchBtn.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // registerBtn
             // 
-            this.button3.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button3.Location = new System.Drawing.Point(907, 489);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(104, 54);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "登録";
-            this.button3.UseVisualStyleBackColor = true;
+            this.registerBtn.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.registerBtn.Location = new System.Drawing.Point(907, 489);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(104, 54);
+            this.registerBtn.TabIndex = 7;
+            this.registerBtn.Text = "登録";
+            this.registerBtn.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // fixBtn
             // 
-            this.button4.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button4.Location = new System.Drawing.Point(1068, 489);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(104, 54);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "修正";
-            this.button4.UseVisualStyleBackColor = true;
+            this.fixBtn.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.fixBtn.Location = new System.Drawing.Point(1068, 489);
+            this.fixBtn.Name = "fixBtn";
+            this.fixBtn.Size = new System.Drawing.Size(104, 54);
+            this.fixBtn.TabIndex = 7;
+            this.fixBtn.Text = "修正";
+            this.fixBtn.UseVisualStyleBackColor = true;
             // 
             // InComing
             // 
@@ -148,20 +148,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
             this.ClientSize = new System.Drawing.Size(1610, 982);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.date);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fixBtn);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.incomingTbox);
+            this.Controls.Add(this.ordercheckBtn);
+            this.Controls.Add(this.incomingDgv);
+            this.Controls.Add(this.incomingDtp);
+            this.Controls.Add(this.itemsLbl);
+            this.Controls.Add(this.dateLbl);
+            this.Controls.Add(this.incomingtitleLbl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "InComing";
             this.Text = "InComing";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.incomingDgv)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -170,16 +170,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label date;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label incomingtitleLbl;
+        private System.Windows.Forms.Label dateLbl;
+        private System.Windows.Forms.DateTimePicker incomingDtp;
+        private System.Windows.Forms.DataGridView incomingDgv;
+        private System.Windows.Forms.Button ordercheckBtn;
+        private System.Windows.Forms.Label itemsLbl;
+        private System.Windows.Forms.TextBox incomingTbox;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.Button fixBtn;
         private System.Windows.Forms.BindingSource bindingSource1;
     }
 }
