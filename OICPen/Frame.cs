32bdbc6ee8cc44131f4603a41b189619541f21b5
b7396 <b7396@5A18>
2018-10-12 14:05:01 +0900

    Form 修正

--- OICPen/Frame.cs
+++ OICPen/Frame.cs
@@ -16,9 +16,9 @@ namespace OICPen
         {
             InitializeComponent();
             /*To know the length and width of the screen*/
-            var f=new login();
-          /*var f = new sales();
-            f.TopLevel = false;
+           // var f=new login();
+          //var f = new sales();
+            /* f.TopLevel = false;
             splitContainer2.Panel2.Controls.Add(f);
             f.Dock = DockStyle.Fill;
             f.Show();
