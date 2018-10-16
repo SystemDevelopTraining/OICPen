998b69e7cbe1d1b5a1fb2bc952caa3f676de8382
b7038 <b7038@5A10>
2018-10-16 16:19:44 +0900

    名称変更 及びデートタイムピッカー追加に追従する箇所の変更

--- OICPen/sales.Designer.cs
+++ OICPen/sales.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.salesLbl = new System.Windows.Forms.Label();
-            this.salesFullBtn = new System.Windows.Forms.Button();
             this.salesSelectBtn = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.salesDgv = new System.Windows.Forms.DataGridView();
             this.rankBtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.salesStartDtp = new System.Windows.Forms.DateTimePicker();
+            this.salesEndDtp = new System.Windows.Forms.DateTimePicker();
+            this.salesBetweenLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.salesDgv)).BeginInit();
             this.SuspendLayout();
             // 
             // salesLbl
@@ -49,17 +51,6 @@
             this.salesLbl.TabIndex = 0;
             this.salesLbl.Text = "売上管理";
             // 
-            // salesFullBtn
-            // 
-            this.salesFullBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.salesFullBtn.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.salesFullBtn.Location = new System.Drawing.Point(1384, 383);
-            this.salesFullBtn.Name = "salesFullBtn";
-            this.salesFullBtn.Size = new System.Drawing.Size(186, 58);
-            this.salesFullBtn.TabIndex = 1;
-            this.salesFullBtn.Text = "売上一覧";
-            this.salesFullBtn.UseVisualStyleBackColor = true;
-            // 
             // salesSelectBtn
             // 
             this.salesSelectBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
@@ -71,15 +62,15 @@
             this.salesSelectBtn.Text = "売上表";
             this.salesSelectBtn.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // salesDgv
             // 
-            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(8, 28);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(1310, 936);
-            this.dataGridView1.TabIndex = 2;
+            this.salesDgv.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.salesDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.salesDgv.Location = new System.Drawing.Point(8, 28);
+            this.salesDgv.Name = "salesDgv";
+            this.salesDgv.RowTemplate.Height = 21;
+            this.salesDgv.Size = new System.Drawing.Size(1310, 936);
+            this.salesDgv.TabIndex = 2;
             // 
             // rankBtn
             // 
@@ -92,22 +83,50 @@
             this.rankBtn.Text = "ランキング";
             this.rankBtn.UseVisualStyleBackColor = true;
             // 
+            // salesStartDtp
+            // 
+            this.salesStartDtp.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.salesStartDtp.Location = new System.Drawing.Point(1370, 741);
+            this.salesStartDtp.Name = "salesStartDtp";
+            this.salesStartDtp.Size = new System.Drawing.Size(200, 36);
+            this.salesStartDtp.TabIndex = 3;
+            // 
+            // salesEndDtp
+            // 
+            this.salesEndDtp.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.salesEndDtp.Location = new System.Drawing.Point(1370, 844);
+            this.salesEndDtp.Name = "salesEndDtp";
+            this.salesEndDtp.Size = new System.Drawing.Size(200, 36);
+            this.salesEndDtp.TabIndex = 4;
+            // 
+            // salesBetweenLbl
+            // 
+            this.salesBetweenLbl.AutoSize = true;
+            this.salesBetweenLbl.Font = new System.Drawing.Font("HG創英ﾌﾟﾚｾﾞﾝｽEB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.salesBetweenLbl.Location = new System.Drawing.Point(1453, 797);
+            this.salesBetweenLbl.Name = "salesBetweenLbl";
+            this.salesBetweenLbl.Size = new System.Drawing.Size(43, 29);
+            this.salesBetweenLbl.TabIndex = 5;
+            this.salesBetweenLbl.Text = "～";
+            // 
             // Sales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
             this.ClientSize = new System.Drawing.Size(1610, 982);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.salesBetweenLbl);
+            this.Controls.Add(this.salesEndDtp);
+            this.Controls.Add(this.salesStartDtp);
+            this.Controls.Add(this.salesDgv);
             this.Controls.Add(this.rankBtn);
             this.Controls.Add(this.salesSelectBtn);
-            this.Controls.Add(this.salesFullBtn);
             this.Controls.Add(this.salesLbl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Sales";
             this.Text = "sales";
             this.Load += new System.EventHandler(this.sales_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesDgv)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,9 +135,11 @@
         #endregion
 
         private System.Windows.Forms.Label salesLbl;
-        private System.Windows.Forms.Button salesFullBtn;
         private System.Windows.Forms.Button salesSelectBtn;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView salesDgv;
         private System.Windows.Forms.Button rankBtn;
+        private System.Windows.Forms.DateTimePicker salesStartDtp;
+        private System.Windows.Forms.DateTimePicker salesEndDtp;
+        private System.Windows.Forms.Label salesBetweenLbl;
     }
 }
