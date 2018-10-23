32b90429e449b6849884d9eee9cd4471fc73c6f8
mugendex <tanariku6134@gmail.com>
2018-10-23 16:50:35 +0900

    テーブル書いた

---
+++ OICPen/Models/Staff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OICPen.Models
+{
+    class Staff
+    {
+        [Key]
+        public int Id { get; set; } // 社員ID
+        [Required]
+        public string Name { get; set; } // 社員名
+        [Required]
+        public string Hurigana { get; set; } // ふりがな
+        [Required]
+        public string Password { get; set; } // パスワード
+        [Required]
+        public string Permission { get; set; } // 権限
+    }
+}
