ab94ad87ff1b52025b20e44abec3578519766dc9
daiki431 <dai.take431@gmail.com>
2018-11-02 16:48:23 +0900

    数量の確認 Nullの場合再入力

--- OICPen/GiveOrder.cs
+++ OICPen/GiveOrder.cs
@@ -21,5 +21,53 @@ namespace OICPen
         {
 
         }
+
+        private void confirmBtn_Click(object sender, EventArgs e)
+        {
+            int ok=0;
+
+            if (quantityMaskedTbox.Text != "")
+            {
+                if (int.Parse(quantityMaskedTbox.Text) >= 1000)
+                {
+
+                    //数量確認メッセージボックスを表示する
+                    DialogResult result = MessageBox.Show("1000個以上の発注になりますがよろしいですか？",
+                        "質問",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Exclamation,
+                        MessageBoxDefaultButton.Button2);
+
+                    //何が選択されたか調べる
+                    if (result == DialogResult.Yes)
+                    {
+                        //「はい」が選択された時
+                        ok = 1;
+                    }
+                    else if (result == DialogResult.No)
+                    {
+                        //「いいえ」が選択された時
+                        quantityMaskedTbox.Focus();
+                    }
+                }
+                else
+                {
+                    //Okay
+                    ok = 1;
+                }
+            }
+            else
+            {
+                //数量がNullの時
+                MessageBox.Show("数量を入力して下さい。","エラー",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            if (ok == 1)
+            {
+                //実行内容
+                MessageBox.Show(quantityMaskedTbox.Text, "GOOD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                quantityMaskedTbox.Text = null;
+
+            }
+        }
     }
 }
