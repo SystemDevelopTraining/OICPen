c3d69b667289a476dd0d2c7d763ceff56aa97c50
mugendex <tanariku6134@gmail.com>
2018-11-06 15:45:43 +0900

    StockServiceの追加

--- OICPen/Services/GiveOrderDetailService.cs
+++ OICPen/Services/GiveOrderDetailService.cs
@@ -7,27 +7,28 @@ using System.Threading.Tasks;
 
 namespace OICPen.Services
 {
-    public class TakeOrderDetailService
+    class GiveOrderDetailService
     {
         private OICPenDbContext context;
 
-        public TakeOrderDetailService(OICPenDbContext context)
+        public GiveOrderDetailService(OICPenDbContext context)
         {
             this.context = context;
         }
 
 
         /*---------------------------------------------------------------
-         [役割] 注文明細の登録
+         [役割] 発注明細の登録
          [引数] item: 登録する商品
                 quantity: 数量
                 order: 登録したい注文
          [返り値] なし
          ---------------------------------------------------------------*/
-        public void AddTakeOrderDetail(Item item, int quantity, Models.TakeOrder order)
+        public void AddTakeOrderDetail(Item item, int quantity, Models.GiveOrder order)
         {
-            var orderDetails = new TakeOrderDetail { ItemId = item.Id, Quantity = (uint)quantity, TakeOrderId = order.Id };
+            var orderDetails = new GiveOrderDetail { Itemid = item.Id, Quantity = quantity, GiveOrderId = order.Id };
             context.SaveChanges();
         }
+
     }
 }
