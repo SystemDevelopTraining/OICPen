c79439f9aab6b4c7ec5b5dbcb0b41e19d36da3cf
b7134 <b7134@5A17>
2018-11-13 16:12:17 +0900

    Hello 123

--- OICPen/Stock.cs
+++ OICPen/Stock.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
+
 
 namespace OICPen
 {
@@ -26,35 +28,80 @@ namespace OICPen
 
         private void searchBtn_Click(object sender, EventArgs e)
         {
-            if (itemsCodeTbox.Text == "" && itemsNameTbox.Text == "")
-            {
-                MessageBox.Show("検索内容を入力してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
+            uint i = 0;
+            string errorMessage = "";
+            string[] search = new string[] { itemsCodeTbox.Text, itemsNameTbox.Text};
+            var checks = new Func<string, string>[] { (x) => "", (x) => ""};
+            search.Zip(checks, (item, check) => {
+                if (!Utility.TextIsEmpty(item))
+                {
+                    i++;
+                    var msg = check(item);
+                    errorMessage += msg == "" ? msg : msg + "\r\n";
+                }
+                return 0;
+            }).ToArray();
+            if (i != 1)
+                errorMessage += "検索項目が１つではありません";
+            if (errorMessage != "")
+            {
+                MessageBox.Show(errorMessage, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
-            else
-                
+            else if (itemsCodeTbox.Text != "" && itemsNameTbox.Text == "")
             {
                 stockDgv.Rows.Clear();
-                foreach (var stock in servis.GetAllStocks())
+                try
                 {
+                    var stockid = servis.NowFindByItemID(int.Parse(itemsCodeTbox.Text));
+                    stockDgv.Rows.Add(stockid.Date,stockid.ItemTID,stockid.ItemT.Name,stockid.Quantity);
+                }
+                catch
+                {
+                  
+                }
+            }
+            else
+            {
+                stockDgv.Rows.Clear();
 
-                    if (itemsCodeTbox.Text == stock.ItemTID.ToString() || itemsNameTbox.Text == stock.ItemT.Name)
-                    {
-                        stockDgv.Rows.Add(stock.Date, stock.ItemTID, stock.ItemT.Name, stock.Quantity);
-                    }
+                var stocks = servis.NowFindByName(itemsNameTbox.Text);
+                foreach (var stock in stocks)
+                {
+                    stockDgv.Rows.Add(stock.Date, stock.ItemTID, stock.ItemT.Name, stock.Quantity);
+
                 }
             }
         }
+      
 
         private void Stock_Load(object sender, EventArgs e)
         {
             foreach (var stock in servis.GetAllStocks())
             {
-                stockDgv.Rows.Add(stock.Date,stock.ItemTID,stock.ItemT.Name,stock.Quantity);
-              
+                stockDgv.Rows.Add(stock.Date, stock.ItemTID, stock.ItemT.Name, stock.Quantity);
+
             }
         }
+
+        private void stockViewBtn_Click(object sender, EventArgs e)
+        {
+            foreach (var stock in servis.GetAllStocks())
+            {
+                stockDgv.Rows.Add(stock.Date, stock.ItemTID, stock.ItemT.Name, stock.Quantity);
+
+            }
+        }
+
+        private void stockDangerViewBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
+
+    
 }
+
            
 
