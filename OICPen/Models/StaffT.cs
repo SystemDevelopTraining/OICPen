7158af18f222d7e3df71f5fd29c6036c0cd81d97
elipmoc <relipmoc101@gmail.com>
2018-11-16 14:45:50 +0900

    社畜サービス実装

--- OICPen/Models/StaffT.cs
+++ OICPen/Models/StaffT.cs
@@ -8,6 +8,21 @@ namespace OICPen.Models
         public string Name { get; set; } // 社員名
         public string Hurigana { get; set; } // ふりがな
         public string Password { get; set; } // パスワード
-        public string Permission { get; set; } // 権限
+        public Permission Permission { get; set; } // 権限
     }
+
+    public enum Permission
+    {
+        //マスター権限。(特権管理者)
+        God = 4,
+        //客管理権限。(客管理者)
+        ClientControl =3,
+        //社員管理権限。(社員管理者)
+        StaffControl =2,
+        //商品管理権限。(商品管理者)
+        ProductControl = 1,
+        //仕入れ管理権限。(入庫管理者)
+        PurchasingControl = 0
+    }
+
 }
